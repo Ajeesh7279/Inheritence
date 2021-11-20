2e7d6708d2b84d42a5cb6228243bbce12dcade1f
Ajeesh7279 <ajeeshas7279@gmail.com>
2021-11-19 23:57:45 -0800

    project Structure

---
+++ src/org/tcs/test/login/CompanyDetails.java
@@ -0,0 +1,22 @@
+package org.tcs.test.login;
+import org.tcs.test.login.*;
+public class CompanyDetails {
+	public void comId() {
+		System.out.println("id is 8956");
+	}
+		
+	public  void comName() {
+	 System.out.println("name is mars");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
---
+++ src/org/tcs/test/login/EmployeeDetails.java
@@ -0,0 +1,33 @@
+package org.tcs.test.login;
+public class EmployeeDetails extends CompanyDetails {
+	
+	private void empId() {
+	System.out.println("id is 6780");
+
+}	
+	
+	private void empName() {
+	System.out.println("name is jupiter");
+
+	}
+	
+	public static void main(String[] args) {
+		EmployeeDetails x=new EmployeeDetails();
+		x.empId();
+		x.empName();
+		x.comId();
+		x.comName();
+		
+	
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
